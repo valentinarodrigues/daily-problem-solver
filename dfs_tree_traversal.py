51d04a2be0c9431a5f7e4f0bed06afe1631c5626
Valentina Rodrigues <valentinarodrigues93@gmail.com>
2021-08-11 15:01:37 +0530

    dfs

---
+++ dfs_tree_traversal.py
@@ -0,0 +1,35 @@
+class Node():
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
+
+def inOrder(root):
+    if(root):
+        inOrder(root.left)    
+        print(root.value)
+        inOrder(root.right)
+
+def preOrder(root):
+    if root:
+        print(root.value)
+        preOrder(root.left)
+        preOrder(root.right)
+
+def postOrder(root):
+    if root:
+        postOrder(root.left)
+        postOrder(root.right)
+        print(root.value)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+preOrder(root)
+print('')
+postOrder(root)
+print('')
+inOrder(root)
