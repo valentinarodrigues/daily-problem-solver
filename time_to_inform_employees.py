6766c40ec6a494c87f538fc7306ac18c670183cd
Valentina Rodrigues <valentinarodrigues93@gmail.com>
2021-08-12 18:15:09 +0530

    dfs employee manager

--- time_to_inform_employees.py
+++ time_to_inform_employees.py
@@ -1,16 +1,33 @@
+from collections import defaultdict
 class Solution:
-    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        graph = defaultdict(list)
-        for i in range(len(manager)):
-            graph[manager[i]].append(i)
-        
-        def dfs(u):
-            ans = 0
-            for v in graph[u]:
-                ans = max(dfs(v) + informTime[u], ans)
-            return ans
-        
+    def numOfMinutes(self, n, headID, manager, informTime):
+        children = defaultdict(list)
+        for i, m in enumerate(manager):
+            if m >= 0: children[m].append(i)
+
+        def dfs(i):
+            # find the max since one employee can manage a heirachy whereas other manager can just manage one employee under him
+            '''
+                    A
+                   / \
+                  B   C
+                     / \
+                    D   E   
+            '''
+            '''
+            [0] is to manage case where the employee does not have to manage other employees i.e leaf node would be [] but max function dies not accept empty list
+            '''
+            return max([dfs(j) for j in children[i]] or [0]) + informTime[i]
         return dfs(headID)
 
+s = Solution()
 
-Input: n = 6, headID = 2, manager = [2,2,-1,2,2,2], informTime = [0,0,1,0,0,0]
+# n = 7
+# headID = 6
+# manager = [1,2,3,4,5,6,-1]
+# informTime = [0,6,5,4,3,2,1]
+n = 15
+headID = 0
+manager = [-1,0,0,1,1,2,2,3,3,4,4,5,5,6,6]
+informTime = [1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]
+print(s.numOfMinutes(n, headID, manager, informTime))
